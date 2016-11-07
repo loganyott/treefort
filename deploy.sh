08c795d46b855b4c36e67a23157a6899270cf3de
junger <junger@idearoomtech.com>
2016-11-06 20:53:58 -0700

    deploy performers and venues

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,9 @@
 
 zip events-api.zip ./src/events-api.js
 aws lambda update-function-code --function-name eventsAPI --zip-file fileb://events-api.zip
+
+zip performers-api.zip ./src/performers-api.js
+aws lambda update-function-code --function-name performersAPI --zip-file fileb://performers-api.zip
+
+zip venues-api.zip ./src/venues-api.js
+aws lambda update-function-code --function-name venuesAPI --zip-file fileb://venues-api.zip

4c6fdacb604d521d81418fe882a1e199ec4bbf4a
junger <junger@idearoomtech.com>
2016-11-06 21:23:21 -0700

    junk the paths so lamba functions are correctly found

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-zip events-api.zip ./src/events-api.js
+zip -j events-api.zip ./src/events-api.js
 aws lambda update-function-code --function-name eventsAPI --zip-file fileb://events-api.zip
 
-zip performers-api.zip ./src/performers-api.js
+zip -j performers-api.zip ./src/performers-api.js
 aws lambda update-function-code --function-name performersAPI --zip-file fileb://performers-api.zip
 
-zip venues-api.zip ./src/venues-api.js
+zip -j venues-api.zip ./src/venues-api.js
 aws lambda update-function-code --function-name venuesAPI --zip-file fileb://venues-api.zip
 
 aws apigateway create-deployment --rest-api-id 7n74ikdn58 --stage-name dev

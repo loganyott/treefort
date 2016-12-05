c98c8a5a7acd46889995ccf023ca2f0ce7da763a
Will Foster <williamgfoster@gmail.com>
2016-12-05 00:32:48 -0700

    no pushd and popd on CircleCI ¯\_(ツ)_/¯

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,13 @@
 zip -j events-api.zip ./src/events-api.js
 aws lambda update-function-code --function-name eventsAPI --zip-file fileb://events-api.zip
 
-pushd src; zip -r ../performers-api.zip ./performers-api.js ./lib ./controllers ../node_modules; popd;
+zip -r ./performers-api.zip ./src/performers-api.js ./src/lib ./src/controllers ./node_modules;
 aws lambda update-function-code --function-name performersAPI --zip-file fileb://performers-api.zip
 
 zip -j venues-api.zip ./src/venues-api.js
 aws lambda update-function-code --function-name venuesAPI --zip-file fileb://venues-api.zip
 
-pushd src; zip -r ../playlists-api.zip ./playlists-api.js ./lib ./controllers ../node_modules; popd;
+zip -r ./playlists-api.zip ./src/playlists-api.js ./src/lib ./src/controllers ./node_modules;
 aws lambda update-function-code --function-name playlistsAPI --zip-file fileb://playlists-api.zip
 
 aws apigateway put-rest-api --rest-api-id 7n74ikdn58 --mode overwrite --body 'file://./src/api-gateway/TreefortAPI-dev-swagger.json'

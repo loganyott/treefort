0c60ddd26dbbb7718a3b490d88630484c5745529
Blake Dietz <blakedietz@gmail.com>
2016-12-10 19:55:12 -0700

    Add tests for the playlist controller get by id method.

--- src/controllers/playlist/playlist-controller.test.js
+++ src/controllers/playlist/playlist-controller.test.js
@@ -14,15 +14,27 @@ const dynamo = new AWS.DynamoDB.DocumentClient();
 // const dynamoDbPerformers = require('./playlist-controller.mock').performer;
 
 describe('PlaylistController', function testPlaylistController() {
-  describe('#get', function testGet() {
+  describe('#get/0', function testGet() {
     it('Creates an object hash of playlists that contains a playlist keyed at 1', function testObjectHash(done) {
       const playlistController = new PlaylistController(dynamo);
       playlistController.get()
-                .then((result) => {
-                  assert.property(result, '1', 'Playlists object contains a playlist that is keyed by 1');
-                  assert.lengthOf(result['1'].tracks, 75, 'Playlist 1 has a length of 75');
-                  done();
-                });
+        .then((result) => {
+          assert.property(result, '1', 'Playlists object contains a playlist that is keyed by 1');
+          assert.lengthOf(result['1'].tracks, 75, 'Playlist 1 has a length of 75');
+          done();
+        });
+    });
+  });
+
+  describe('#get/1', function testGetWithId() {
+    it('Returns a single object that matches the current id.', function getPlaylistWithKey1(done) {
+      const playlistController = new PlaylistController(dynamo);
+      playlistController.get('1')
+        .then((result) => {
+          assert.equal(result.id, 1);
+          assert.lengthOf(result.tracks, 75, 'Playlist 1 has a length of 75');
+          done();
+        });
     });
   });
 });

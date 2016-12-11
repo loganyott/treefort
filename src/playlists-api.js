d7cc300fb1b4a56f4cfda85ced1ac3689e8be275
Blake Dietz <blakedietz@gmail.com>
2016-12-10 19:30:27 -0700

    Add support for getting playlists by id.

--- src/playlists-api.js
+++ src/playlists-api.js
@@ -2,31 +2,37 @@
 
 console.log('Loading function');
 
+// eslint-disable-next-line import/no-extraneous-dependencies
 const AWS = require('aws-sdk');
-const dynamo = new AWS.DynamoDB.DocumentClient();
 const response = require('./lib/response');
 const PlaylistController = require('./controllers/playlist/playlist-controller').PlaylistController;
 
 console.log('Requires completed');
 
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
 exports.handler = (event, context, callback) => {
-    console.log('Received event:', JSON.stringify(event, null, 2));
+  console.log('Received event:', JSON.stringify(event, null, 2));
 
-    const playlistController = new PlaylistController(dynamo);
-    const done = response(callback);
+  const playlistController = new PlaylistController(dynamo);
+  const done = response(callback);
 
-    switch (event.httpMethod) {
-        case 'GET':
-            console.log('in get');
-            playlistController.get()
-                .then((response) => {
-                    done(null, response);
-                })
-                .catch((error) => {
-                    done(error);
-                });
-            break;
-        default:
-            done(new Error(`Unsupported method "${event.httpMethod}"`));
-    }
+  let pathParameters = null;
+
+  switch (event.httpMethod) {
+    case 'GET':
+      if (event.pathParameters && event.pathParameters.playlistId) {
+        pathParameters = event.pathParameters.playlistId;
+      }
+
+      playlistController
+        .get(pathParameters)
+        .then(getResponse => done(null, getResponse))
+        .catch(error => done(error));
+
+      break;
+    default:
+      done(new Error(`Unsupported method "${event.httpMethod}"`));
+      break;
+  }
 };

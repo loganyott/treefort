52a6b9667250fe3ceac079cb98ccf0ce256c002e
Blake Dietz <blakedietz@gmail.com>
2017-01-08 15:25:30 -0700

    Split dynamo table out of dynamo promise. Add a batch put function for dynamo table.

--- src/lib/dynamo-promise.js
+++ src/lib/dynamo-promise.js
@@ -1,69 +1,7 @@
 'use strict';
 
 const Promise = require('bluebird');
-
-class DynamoTable {
-  constructor(dynamoConnection, tableName) {
-    this.dynamo = dynamoConnection;
-    this.tableName = tableName;
-  }
-
-  put(item) {
-    const putParams = {
-      TableName: this.tableName,
-      Item: item,
-    };
-
-    return new Promise((resolve, reject) => {
-      this.dynamo
-        .put(putParams, (putError, putResponse) => {
-          if (putError) {
-            reject(putError);
-          } else {
-            resolve(putResponse.Item);
-          }
-        });
-    });
-  }
-
-  scan() {
-    const scanParams = {
-      TableName: this.tableName,
-    };
-
-    return new Promise((resolve, reject) => {
-      this.dynamo
-        .scan(scanParams, (scanError, scanResponse) => {
-          if (scanError) {
-            reject(scanError);
-          } else {
-            resolve(scanResponse.Items);
-          }
-        });
-    });
-  }
-
-  // TODO: (bdietz) this could be better supported to match the aws dynamo db more robustly.
-  get(id) {
-    const getParams = {
-      TableName: this.tableName,
-      Key: {
-        id,
-      },
-    };
-
-    return new Promise((resolve, reject) => {
-      this.dynamo
-        .get(getParams, (getError, getResponse) => {
-          if (getError) {
-            reject(getError);
-          } else {
-            resolve(getResponse.Item);
-          }
-        });
-    });
-  }
-}
+const DynamoTable = require('./dynamo-table');
 
 class DynamoPromise {
   constructor(dynamoConnection) {
---
+++ src/lib/dynamo-table.js
@@ -0,0 +1,72 @@
+'use strict';
+
+class DynamoTable {
+  constructor(dynamoConnection, tableName) {
+    this.dynamo = dynamoConnection;
+    this.tableName = tableName;
+  }
+
+  batchPut(items) {
+    const batchPutPromises = items.map(item => this.put(item));
+
+    return Promise.all(batchPutPromises);
+  }
+
+  put(item) {
+    const putParams = {
+      TableName: this.tableName,
+      Item: item,
+    };
+
+    return new Promise((resolve, reject) => {
+      this.dynamo
+        .put(putParams, (putError, putResponse) => {
+          if (putError) {
+            reject(putError);
+          } else {
+            resolve(putResponse.Item);
+          }
+        });
+    });
+  }
+
+  scan() {
+    const scanParams = {
+      TableName: this.tableName,
+    };
+
+    return new Promise((resolve, reject) => {
+      this.dynamo
+        .scan(scanParams, (scanError, scanResponse) => {
+          if (scanError) {
+            reject(scanError);
+          } else {
+            resolve(scanResponse.Items);
+          }
+        });
+    });
+  }
+
+  // TODO: (bdietz) this could be better support dynamo db's api.
+  get(id) {
+    const getParams = {
+      TableName: this.tableName,
+      Key: {
+        id,
+      },
+    };
+
+    return new Promise((resolve, reject) => {
+      this.dynamo
+        .get(getParams, (getError, getResponse) => {
+          if (getError) {
+            reject(getError);
+          } else {
+            resolve(getResponse.Item);
+          }
+        });
+    });
+  }
+}
+
+module.exports = DynamoTable;

b2b52b130ac0548326029b0a98f3248dd487d21d
Will Foster <williamgfoster@gmail.com>
2017-03-04 15:48:06 -0800

    One more deploy change
    Better output

--- deploy.sh
+++ deploy.sh
@@ -23,28 +23,28 @@ cd ..;
 aws lambda update-function-code --function-name etl --zip-file fileb://etl.zip;
 aws lambda update-function-configuration --function-name etl --timeout 60 --memory-size 512
 
-echo "Deploying events-api lambda function";
+echo "Deploying ${AWS_STAGE}-events-api lambda function";
 cd src;
 zip -r ../events-api.zip ./events-api.js ./lib ./controllers ../node_modules > /dev/null;
 cd ..;
 aws lambda update-function-code --function-name ${AWS_STAGE}-events-api --zip-file fileb://events-api.zip;
 aws lambda update-function-configuration --function-name ${AWS_STAGE}-events-api --timeout 3 --memory-size 512
 
-echo "Deploying lines-api lambda function";
+echo "Deploying ${AWS_STAGE}-lines-api lambda function";
 cd src
 zip -r ../lines-api.zip ./lines-api.js ./lib ./controllers ../node_modules > /dev/null
 cd ..
 aws lambda update-function-code --function-name ${AWS_STAGE}-lines-api --zip-file fileb://lines-api.zip;
 aws lambda update-function-configuration --function-name ${AWS_STAGE}-lines-api --timeout 3 --memory-size 512
 
-echo "Deploying performers-api lambda function";
+echo "Deploying ${AWS_STAGE}-performers-api lambda function";
 cd src;
 zip -r ../performers-api.zip ./performers-api.js ./lib ./controllers ../node_modules > /dev/null;
 cd ..;
 aws lambda update-function-code --function-name ${AWS_STAGE}-performers-api --zip-file fileb://performers-api.zip;
 aws lambda update-function-configuration --function-name ${AWS_STAGE}-performers-api --timeout 3 --memory-size 512
 
-echo "Deploying playlists-api lambda function";
+echo "Deploying ${AWS_STAGE}-playlists-api lambda function";
 cd src
 zip -r ../playlists-api.zip ./playlists-api.js ./lib ./controllers ../node_modules > /dev/null
 cd ..

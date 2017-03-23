d9a4fced7c2f11bba532a3879466512932436901
Blake Dietz <blakedietz@gmail.com>
2017-03-22 20:33:33 -0600

    Include moment-timezone

--- src/lib/dynamo-query.js
+++ src/lib/dynamo-query.js
@@ -1,5 +1,5 @@
 'use strict';
-const moment = require('moment');
+const moment = require('moment-timezone');
 const _ = require('lodash');
 
 const restrictedKeys = [
@@ -12,7 +12,7 @@ const createSetStatement = propertyString => `${propertyString} = ${convertPrope
 const createDynamoPatchQuery = (primaryKeys, _propertiesToUpdate) => {
   // It's kind of weird ahving this in there with the modified timestamp being specific to Boise, but some of the client
   // team wanted it this way... I think that we should change how we do this in the future
-  const propertiesToUpdate = Object.assign({}, _propertiesToUpdate, { updated: moment.tz('America/Boise').format('YYYY-MM-DDThh:mm') });
+  const propertiesToUpdate = Object.assign({}, _propertiesToUpdate, { updated: moment().tz('America/Boise').format('YYYY-MM-DDThh:mm') });
 
   const keysToUpdate = _.keys(propertiesToUpdate)
     .filter((keyName) => {

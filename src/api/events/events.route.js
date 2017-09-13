608a7c6490f6019f28e6eccd95af1806dad5279c
Blake Dietz <blakedietz@gmail.com>
2017-09-12 23:10:47 -0600

    Add some todos.

--- src/api/events/events.route.js
+++ src/api/events/events.route.js
@@ -8,12 +8,15 @@ const dynamo = new AWS.DynamoDB.DocumentClient();
 const router = (event, context, callback) => {
   console.log('Received event:', JSON.stringify(event, null, 2));
 
+  // TODO: (bdietz) - Make sure that this can be dynamic again
   // const eventController = new EventController(dynamo, event.stageVariables.db_stage, event.stageVariables.current_wave);
   const eventController = new EventController(dynamo, 'dev', 5);
   const done = response(callback);
 
   let pathParameters = null;
 
+  // TODO: (bdietz) - Need to update the rest of the endpoints to reference the newly namespaced event object, may also need to
+  // make sure that the other parameters that are being referenced have not changed location in the namespace.
   switch (event.method) {
     case 'GET':
       if (event.pathParameters && event.pathParameters.eventId) {

b91f43e885f8ffb3f557e3e6bb384c1e7fb70275
Blake Dietz <blakedietz@gmail.com>
2017-09-12 23:13:19 -0600

    Add some comments about how the webpack.config.js works.

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 const slsw = require('serverless-webpack');
 
-
 module.exports = {
+  // Allows for dynamic entry files based upon what is in the serverless.yml. That means one source of truth!
   entry: slsw.lib.entries,
   externals: [nodeExternals()],
   target: 'node',

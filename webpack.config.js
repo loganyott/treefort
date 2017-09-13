7033cd3248eb1f7c0d1da047a9eaa6fbcf859c42
Blake Dietz <blakedietz@gmail.com>
2017-09-12 23:13:19 -0600

    Add some comments about how the webpack.config.js works.

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,14 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 const slsw = require('serverless-webpack');
 
-
 module.exports = {
+  // Allows for dynamic entry files based upon what is in the serverless.yml. That means one source of truth!
   entry: slsw.lib.entries,
+  /*
+   * Here we completely ignore adding all of the files in node_modules because we assume that they'll be available in
+   * the runtime. The files are imported by serverless webpack due to the configuration `webpackIncludeModules: true`
+   * and are loaded into the node environment at runtime.
+   */
   externals: [nodeExternals()],
   target: 'node',
   module: {
